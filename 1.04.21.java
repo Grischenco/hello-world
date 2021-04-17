d937f55e95f744e0d673c54f19e825c390ca7a9c
Grischenco <81438686+Grischenco@users.noreply.github.com>
2021-04-17 20:55:18 +0300

    Update 1.04.21.java

--- 1.04.21.java
+++ 1.04.21.java
@@ -1,51 +1,129 @@
-public class Hotel {
-    Room[] freeRooms;
-    int[] usedRooms;
-    Hotel(Room[] rooms){
-        this.freeRooms =  rooms;
-        this.usedRooms = new int[rooms.length];
-    }
-    void getFreeRooms(){
-        Room[] rooms = this.freeRooms;
-        System.out.println("--Список свободных комнат--");
-        for (int i = 0; i < rooms.length; i++) {
-            if (rooms[i].roomNumber==0) {
-                continue;
+// пример кода _ ворованный
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+import static Main.checkProxy;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+            FileReader reader = new FileReader("D:\\Програмирование\\Уроки\\text.txt");
+            int c;
+            String ip = "";
+            String port = "";
+            String proxy = "";
+            while ((c = reader.read()) != -1) {
+                if (c == 13) continue;
+                else if (c == 10) {
+                    ip = proxy.split(":")[0];
+                    port = proxy.split(":")[1];
+                    /*proxy = "";
+                    String finalIp = ip;
+                    String finalPort = port;
+                    Thread thread = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            String result = checkProxy(finalIp, Integer.parseInt(finalPort));
+                            if(result.equals("error"))
+                                System.out.println(finalIp+" не работает");
+                            else {
+                                System.out.println(finalIp+ " работает");
+                            }
+                        }
+                    });
+                    thread.start();
+                }*/
+
+                    //Способ 1
+
+                    /*MyThread thread = new MyThread();
+                    proxy = "";
+                    thread.finalIp = ip;
+                    thread.finalPort = port;
+                    thread.start();
+                }*/
+
+                    // Способ 2
+
+                    /*Thread thread = new Thread(new MyRunnableClass(port,ip));
+                    proxy = "";
+                    thread.start();
+                }*/
+
+                else if (c != 9) {
+                    proxy += (char) c;
+                } else {
+                    proxy += ":";
+                }
             }
-            System.out.println("Комната №"+rooms[i].roomNumber);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        System.out.println("--Конец списка--");
+
     }
-    void getUsedRooms(){
-        int[] rooms = this.usedRooms;
-        System.out.println("--Список занятых комнат--");
-        for (int i = 0; i < rooms.length; i++) {
-            if (rooms[i]==0) {
-                continue;
+
+    static String checkProxy(String ip, int port) {
+        HttpURLConnection connection = null;
+        try {
+            Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(ip, port));
+            URL url = new URL("https://vozhzhaev.ru/test.php");
+            connection = (HttpURLConnection) url.openConnection(proxy);
+            InputStream is = connection.getInputStream();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+            StringBuilder response = new StringBuilder(); // or StringBuffer if Java version 5+
+            String line;
+            while ((line = rd.readLine()) != null) {
+                response.append(line);
+                response.append('\r');
             }
-            System.out.println("Комната №"+rooms[i]);
+            rd.close();
+            return response.toString();
+        } catch (Exception e) {
+            return "error";
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
         }
-        System.out.println("--Конец списка--");
     }
-    void reservedRoom(int needRoom){
-        Room[] freeRooms = this.freeRooms; //3
-        for (int i = 0; i < freeRooms.length; i++) {
-            if(needRoom == freeRooms[i].roomNumber){
-                freeRooms[i].roomNumber = 0;
-                this.usedRooms[i] = needRoom;
-                System.out.println("Комната номер "+needRoom+" свободна");
-                break;
-            }else if(i+1==freeRooms.length){
-                System.out.println("Такой комныты не найдено");
-            }
+}
+
+class MyThread extends Thread {
+    String result;
+    String finalPort;
+    String finalIp;
+
+    @Override
+    public void run() {
+        result = checkProxy(finalIp, Integer.parseInt(finalPort));
+        if (result.equals("error"))
+            System.out.println(finalIp + " не работает");
+        else {
+            System.out.println(finalIp + " работает");
         }
     }
-    void getRoomsWithEat(){
-        Room[] rooms = this.freeRooms;
-        for (int i = 0; i < rooms.length; i++) {
-            if(rooms[i].eat && rooms[i].roomNumber!=0){
-                System.out.println("Комната с едой "+rooms[i].roomNumber);
-            }
+}
+
+class MyRunnableClass implements Runnable {
+    String result;
+    String finalPort;
+    String finalIp;
+
+    public MyRunnableClass(String finalPort, String finalIp) {
+        this.finalPort = finalPort;
+        this.finalIp = finalIp;
+    }
+
+
+    @Override
+    public void run() {
+        result = checkProxy(finalIp, Integer.parseInt(finalPort));
+        if (result.equals("error"))
+            System.out.println(finalIp + " не работает");
+        else {
+            System.out.println(finalIp + " работает");
         }
     }
 }
