097797cde91447b432ed9b003b790cf60e903586
Grischenco <81438686+Grischenco@users.noreply.github.com>
2021-04-22 21:42:19 +0300

    Update User.java

--- chat/сервер/User.java
+++ chat/сервер/User.java
@@ -1,1 +1,27 @@
+ сервер пакетов ;
 
+import  java.io.DataOutputStream ;
+import  java.io.IOException ;
+import  java.net.Socket ;
+import  java.util.UUID ;
+
+public  class  User {
+    private  String userName;
+    частный  сокет Socket ;
+    частный  UUID uuid;
+
+    общедоступный  пользователь ( String  userName , Socket  socket ) {
+        это . userName = имя пользователя ;
+        это . socket = сокет;
+    }
+    public  String  getUserName () { return userName;}
+    общедоступный  сокет  getSocket () { возвратный сокет; }
+    public  void  sendMessage ( String  msg ) {
+        попробуйте {
+            DataOutputStream out =  новый  DataOutputStream ( this . Socket . GetOutputStream ());
+            из . writeUTF (сообщение);
+        } catch ( IOException e) {
+            е . printStackTrace ();
+        }
+    }
+}
